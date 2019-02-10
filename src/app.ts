1150650836d250bac1d6b98d7bd3943f17deaf7c
Matt Hilton <matt@mjhilton.net>
2019-02-10 17:35:13 +1100

    Remove express, we're not building a web server

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express = require('express');
+
+const app: express.Application = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
