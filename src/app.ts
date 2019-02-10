93296c01135553c66e2d83d0b0a73fece9e8afa1
Matt Hilton <matt@mjhilton.net>
2019-02-10 18:55:49 +1100

    Add node-ts to transpile and execute TS files in memory. Add example of an institution file executing Puppeteer

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 import program = require('commander');
+import tsnode = require('ts-node');
+import fs = require('fs');
 
 program
     .arguments('<institution>')
     .option('-u, --username <username>', 'The user to authenticate as')
     .option('-p, --password <password>', 'The user\'s password')
     .action(function(institution) {
-        console.log('Institution: %s; User: %s; Passowrd: %s.', 
-            institution, program.username, program.password);
+        var institutionName = institution + '.ts';
+        var institutionPath = '../institutions/' + institutionName;
+        
+        console.log("Running institution %s (%s)", institution, institutionPath);
+
+        tsnode.register({ 
+            skipProject: true,
+            transpileOnly: true
+        });
+
+        require(institutionPath);
     })
     .parse(process.argv);
