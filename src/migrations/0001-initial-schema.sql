1bacbc7b838c5bddd0672d8f77c3871df3068d23
Tatham Oddie <tatham@oddie.com.au>
2019-02-22 23:14:28 +1100

    Reorder columns; least specific identifier first

--- src/migrations/0001-initial-schema.sql
+++ src/migrations/0001-initial-schema.sql
@@ -3,9 +3,9 @@
 CREATE TABLE Balances
 (
     id INTEGER PRIMARY KEY,
+    institution TEXT,
     accountNumber TEXT,
     accountName TEXT,
-    institution TEXT,
     balance INTEGER,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
