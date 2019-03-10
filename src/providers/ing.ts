e6915664e07df4c3aab945ab22aa00e5f4a338a0
Tatham Oddie <tatham@oddie.com.au>
2019-03-10 16:07:49 +1100

    Extract common logging code; add better comments

--- src/providers/ing.ts
+++ src/providers/ing.ts
@@ -20,6 +20,8 @@ export class Ing implements BankDataProviderInterface {
     ) {
         this.browser = await puppeteer.launch({
             headless: !this.executionContext.debug,
+
+            // ING's banking is heavily client-side, with dynamic Polymer components popping in and out
             slowMo: 100
         });
         var page = this.page = await this.browser.newPage();
@@ -29,50 +31,39 @@ export class Ing implements BankDataProviderInterface {
 
         await page.goto("https://www.ing.com.au/securebanking/");
         await page.waitForSelector('#cifField');
-        if (this.executionContext.debug) {
-            console.log('1');
-        }
+        this.debugLog('login', 1);
+
+        // Fill the username, then tab out to trigger their client-side validation
         await page.type('#cifField', username);
-        if (this.executionContext.debug) {
-            console.log('2');
-        }
         await page.keyboard.press('Tab');
-        if (this.executionContext.debug) {
-            console.log('3');
-        }
+        this.debugLog('login', 2);
+
         // Click the secret pixel that fires up the accessible login form
+        // For some reason, puppeteer's native page.click doesn't achieve the same result as evaluating in-page
         await page.evaluate(() => {
             var button = <any>document.querySelector('.ing-login-input input[type="image"].accessibleText');
-            if (button != null)
-                button.click();
+            if (button != null) { button.click(); }
         });
-        //await page.click('.ing-login-input input[type="image"].accessibleText');
         await page.waitForSelector('.ing-accessible-login input[alt="1"]');
-        if (this.executionContext.debug) {
-            console.log('4');
-        }
-        // Type the PIN digit-by-digit
+        this.debugLog('login', 3);
+
+        // Type the PIN digit-by-digit on their virtual keypad
         for (const digit of password) {
             await page.evaluate((d) => {
                 var button = <any>document.querySelector('.ing-accessible-login input[alt="' + d + '"]');
-                if (button != null)
-                    button.click();
+                if (button != null) { button.click(); }
             }, digit);
-            if (this.executionContext.debug) {
-                console.log('5');
-            }
+            this.debugLog('login', 4);
         }
-        if (this.executionContext.debug) {
-            console.log('6');
-        }
+        this.debugLog('login', 5);
+
+        // Click the login button
         await page.evaluate(() => {
             var button = <any>document.querySelector('.ing-accessible-login input[alt="Login"]');
             if (button != null)
                 button.click();
         });
-        if (this.executionContext.debug) {
-            console.log('7');
-        }
+        this.debugLog('login', 6);
     }
 
     async logout() {
@@ -82,16 +73,12 @@ export class Ing implements BankDataProviderInterface {
 
         await page.evaluate(() => {
             var button = <any>document.querySelector('button.uia-logout');
-            if (button != null)
-                button.click();
+            if (button != null) { button.click(); }
         });
-        if (this.executionContext.debug) {
-            console.log('10');
-        }
+        this.debugLog('logout', 1);
+
         await page.waitForSelector('.login-button');
-        if (this.executionContext.debug) {
-            console.log('11');
-        }
+        this.debugLog('login', 2);
 
         await this.browser.close();
     }
@@ -103,9 +90,13 @@ export class Ing implements BankDataProviderInterface {
 
         const balances = new Array<AccountBalance>();
 
-        await page.waitForSelector('ing-page-block.ing-all-accounts-summary');
-        if (this.executionContext.debug) { console.log('8'); }
+        this.debugLog('getBalances', 0);
 
+        // ING uses web components / Polymer, so we get nice and stable tag names
+        await page.waitForSelector('ing-page-block.ing-all-accounts-summary');
+        this.debugLog('getBalances', 1);
+
+        // ING seem to use 'uia-' as a prefix for their own UI Automation hooks; great for us!
         var accountSummaryRows = await page.$$('ing-page-block.ing-all-accounts-summary .uia-account-row');
         for (const row of accountSummaryRows) {
             balances.push({
@@ -117,5 +108,11 @@ export class Ing implements BankDataProviderInterface {
         }
 
         return balances;
+    }
+
+    private debugLog(stage: string, position: number) {
+        if (this.executionContext.debug) {
+            console.log('%s: %s', stage, position.toString());
+        }
     }
 }
