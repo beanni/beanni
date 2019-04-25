8d21bb4a08f6568461299087d0442f3344eed0af
Matt Davies <matt@mdavies.net>
2019-04-25 16:10:17 +1000

    Download ANZ statements

--- src/providers/anz.ts
+++ src/providers/anz.ts
@@ -1,13 +1,16 @@
+import fs = require("fs");
 import puppeteer = require("puppeteer");
+import request = require("request");
 import { IBeanniExecutionContext } from "../core";
 import {
     IAccountBalance,
+    IBankDataDocumentProviderInterface,
     IBankDataProviderInterface,
 } from "../types";
 
 const providerName = "ANZ";
 
-export class Anz implements IBankDataProviderInterface {
+export class Anz implements IBankDataProviderInterface, IBankDataDocumentProviderInterface {
     public executionContext: IBeanniExecutionContext;
 
     public browser: puppeteer.Browser | undefined;
@@ -81,5 +84,86 @@ export class Anz implements IBankDataProviderInterface {
         }
 
         return balances;
+    }
+
+    public async getDocuments(statementFolderPath: string): Promise<void> {
+        if (this.page == null) { throw new Error("Not logged in yet"); }
+        const page = this.page;
+
+        await page.waitForSelector('a[title="View statements"]');
+        await page.waitFor(1000);
+        await page.click('a[title="View statements"]');
+        await page.waitForSelector('select[name="DCC_ACCOUNT_INDEX"] option');
+        let accountSelector = await page.$$('select[name="DCC_ACCOUNT_INDEX"] option');
+
+        for (let i = 0; i < accountSelector.length; i++) {
+            if (i > 0) {
+                await page.select('select[name="DCC_ACCOUNT_INDEX"]', i.toString());
+                await page.waitFor(1000);
+                await page.waitForSelector('select[name="DCC_ACCOUNT_INDEX"] option');
+                accountSelector = await page.$$('select[name="DCC_ACCOUNT_INDEX"] option');
+            }
+
+            const prop = await (await accountSelector[i].getProperty("innerText")).jsonValue();
+            const accountPrefix = prop.match(/\((\d{4})/);
+            const accountSuffix = prop.match(/(\d{4})\)/);
+            const accountNumber = accountPrefix.length > 1 && accountSuffix.length > 1
+                ? `${accountPrefix[1]}xxxxxxxx${accountSuffix[1]}`
+                : prop;
+
+            let year = "";
+            const statements = await page.$$(".monthYearDisplay, #statementTableRow a");
+
+            for (const statement of statements) {
+                if (await (await statement.getProperty("tagName")).jsonValue() === "DIV") {
+                    year = await (await statement.getProperty("innerText")).jsonValue();
+                    continue;
+                }
+
+                const statementUrl = await (await statement.getProperty("href")).jsonValue();
+                const dayDiv = await statement.$$(".dateSectionDiv");
+                const monthDiv = await statement.$$(".monthSectionDiv");
+
+                const day = await (await dayDiv[0].getProperty("innerText")).jsonValue();
+                const month = await (await monthDiv[0].getProperty("innerText")).jsonValue();
+                const filename = `${year}-${month}-${day} ANZ ${accountNumber} Statement.pdf`;
+
+                const targetPath = statementFolderPath + `${filename}`;
+
+                const exists = await new Promise<boolean>((resolve, reject) => {
+                    fs.access(targetPath, fs.constants.F_OK, (err) => {
+                        resolve(err === null);
+                    });
+                });
+                if (exists) {
+                    console.log(`[Anz] Skipping download; already on disk: ${filename}`);
+                    continue;
+                }
+
+                const cookie = await page.evaluate(() => document.cookie);
+                await new Promise((resolve, reject) => {
+                    const file = fs.createWriteStream(targetPath);
+                    request
+                        .get({
+                            uri: statementUrl,
+                            rejectUnauthorized: false,
+                            headers: {
+                                "Referer": page.url(),
+                                "Cookie": cookie,
+                                // tslint:disable-next-line:max-line-length
+                                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+                            },
+                        })
+                        .on("response", (res) => {
+                            res.on("close", () => {
+                                file.close();
+                                console.log(`[Anz] Statement downloaded: ${filename}`);
+                                resolve();
+                            });
+                        })
+                        .pipe(file);
+                });
+            }
+        }
     }
 }
