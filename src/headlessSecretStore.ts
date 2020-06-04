3bd77de62dc6db3fa5448619974d2c333f983fcd
Tatham Oddie <tatham@oddie.com.au>
2020-06-04 22:09:39 +1000

    Show example headless secrets file if empty

--- src/headlessSecretStore.ts
+++ src/headlessSecretStore.ts
@@ -9,6 +9,12 @@ export class HeadlessSecretStore implements ISecretStore {
     constructor(path: string) {
         const configFileText = fs.readFileSync(path, "utf8");
         const fileData =  yaml.safeLoad(configFileText);
+
+        if (fileData === undefined) {
+            console.log("The secrets file is empty. Example format to use:\n\n```\nsecrets:\n\t- key: Westpac:username\n\tvalue: \"12345678\"\n\t- key: Westpac:password\n\tvalue: \"abcdef\"\n```\n");
+            return;
+        }
+
         fileData.secrets.forEach((s: any) => {
             this.data[s.key] = s.value;
         });
