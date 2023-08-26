cb8ffc40a47941cf5055aac793d9ecb8f8cbdadd
Tatham Oddie <tatham@oddie.com.au>
2023-08-26 17:46:49 +1000

    TelstraSuper: add screenshots

--- src/providers/TelstraSuper.ts
+++ src/providers/TelstraSuper.ts
@@ -6,6 +6,7 @@ import {
   IBankDataProviderInterface,
   ValueType,
 } from "../types";
+import { ProviderHelpers } from "../providerHelpers";
 
 export class TelstraSuper implements IBankDataProviderInterface {
   public institution = "TelstraSuper";
@@ -29,19 +30,24 @@ export class TelstraSuper implements IBankDataProviderInterface {
     const username = await retrieveSecretCallback("username");
     const password = await retrieveSecretCallback("password");
 
-    await page.goto("https://www.telstrasuper.com.au/login");
-    await page.waitForSelector("input[type=text][name=MemberNumber]");
-    this.debugLog("login", 1);
+    try {
+      await page.goto("https://www.telstrasuper.com.au/login");
+      await page.waitForSelector("input[type=text][name=MemberNumber]");
+      this.debugLog("login", 1);
 
-    await page.type("input[type=text][name=MemberNumber]", username);
-    await page.type("input[type=password]", password);
-    this.debugLog("login", 2);
+      await page.type("input[type=text][name=MemberNumber]", username);
+      await page.type("input[type=password]", password);
+      this.debugLog("login", 2);
 
-    await page.click("form[action='/login'] button[type=submit]");
-    this.debugLog("login", 3);
+      await page.click("form[action='/login'] button[type=submit]");
+      this.debugLog("login", 3);
 
-    await page.waitForSelector("main#main header h1");
-    this.debugLog("login", 4);
+      await page.waitForSelector("main#main header h1");
+      this.debugLog("login", 4);
+    } catch (error) {
+      await ProviderHelpers.logError(error, page, this.institution);
+      throw error;
+    }
   }
 
   public async logout(): Promise<void> {
@@ -50,8 +56,13 @@ export class TelstraSuper implements IBankDataProviderInterface {
     }
     const page = this.page;
 
-    await page.goto("https://www.telstrasuper.com.au/api/account/logout");
-    this.debugLog("logout", 1);
+    try {
+      await page.goto("https://www.telstrasuper.com.au/api/account/logout");
+      this.debugLog("logout", 1);
+    } catch (error) {
+      await ProviderHelpers.logError(error, page, this.institution);
+      throw error;
+    }
 
     await this.browser.close();
   }
@@ -66,38 +77,43 @@ export class TelstraSuper implements IBankDataProviderInterface {
 
     this.debugLog("getBalances", 0);
 
-    await page.goto("https://www.telstrasuper.com.au/your-account");
-    this.debugLog("getBalances", 1);
+    try {
+      await page.goto("https://www.telstrasuper.com.au/your-account");
+      this.debugLog("getBalances", 1);
 
-    await page.waitForSelector(
-      ".bx-balance-breakdown table.js-chart-data-table tbody tr"
-    );
-    this.debugLog("getBalances", 2);
+      await page.waitForSelector(
+        ".bx-balance-breakdown table.js-chart-data-table tbody tr"
+      );
+      this.debugLog("getBalances", 2);
 
-    const rows = await page.$$(
-      ".bx-balance-breakdown table.js-chart-data-table tbody tr"
-    );
-    this.debugLog("getBalances", 3);
-    for (const row of rows) {
-      const cells = await row.$$("td");
-      this.debugLog("getBalances", 4);
+      const rows = await page.$$(
+        ".bx-balance-breakdown table.js-chart-data-table tbody tr"
+      );
+      this.debugLog("getBalances", 3);
+      for (const row of rows) {
+        const cells = await row.$$("td");
+        this.debugLog("getBalances", 4);
 
-      const accountName = await cells[0].evaluate((el) => el.textContent);
-      assert(accountName);
-      const balanceText = await cells[1].evaluate((el) => el.textContent);
-      assert(balanceText);
-      const accountNumber = await cells[3].evaluate((el) => el.textContent);
-      assert(accountNumber);
+        const accountName = await cells[0].evaluate((el) => el.textContent);
+        assert(accountName);
+        const balanceText = await cells[1].evaluate((el) => el.textContent);
+        assert(balanceText);
+        const accountNumber = await cells[3].evaluate((el) => el.textContent);
+        assert(accountNumber);
 
-      const balance = parseFloat(balanceText);
+        const balance = parseFloat(balanceText);
 
-      balances.push({
-        institution: this.institution,
-        accountName: accountName,
-        accountNumber: accountNumber,
-        balance: balance,
-        valueType: ValueType.Superannuation,
-      });
+        balances.push({
+          institution: this.institution,
+          accountName: accountName,
+          accountNumber: accountNumber,
+          balance: balance,
+          valueType: ValueType.Superannuation,
+        });
+      }
+    } catch (error) {
+      await ProviderHelpers.logError(error, page, this.institution);
+      throw error;
     }
 
     this.debugLog("getBalances", 5);
